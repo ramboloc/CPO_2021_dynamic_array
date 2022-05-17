75ac443aceb76ff59ecb3f1706fba3f3426c6fb5
Bin <1255985865@qq.com>
2022-05-17 09:45:21 +0800

    add function

--- dynamic_array.py
+++ dynamic_array.py
@@ -16,7 +16,7 @@ def from_list(lst: List[Optional[int]]) -> Any:
 
 def concat(dynamic_array1: 'DynamicArray', dynamic_array2: 'DynamicArray') -> 'DynamicArray':
     """ Merge two dynamic arrays """
-    new_dynamic = DynamicArray(dynamic_array1.length() + dynamic_array2.length())
+    new_dynamic = DynamicArray(.length() + dynamic_array2.length())
     for i in dynamic_array1.to_list():
         new_dynamic.cons(i)
     for k in dynamic_array2.to_list():
@@ -46,6 +46,134 @@ class DArrayIterator(object):
         return self.__chunk[self.__index]
 
 
+def iterator(self) -> 'DArrayIterator':
+    """
+    Convert a dynamic array to an iterator
+    :return: an iterator
+    """
+    return DArrayIterator(self.__chunk)
+
+def copy_self(self) -> 'DynamicArray':
+    """
+    Make a copy of self
+    :return:
+    """
+    new_dynamic = copy.deepcopy(self)
+    return new_dynamic
+
+def to_list(self) -> List[int]:
+    """ Transform the array to a list """
+    lst: List[int] = []
+    for i in self.__chunk:
+        if i is not None:
+            lst.append(i)
+        else:
+            break
+    return lst
+
+def cons(self, element: Optional[int]) -> 'DynamicArray':
+    """ Add an element at the end of the array"""
+    new_dynamic = self.copy()
+    if new_dynamic.__size == new_dynamic.__capacity:
+        new_dynamic.__capacity = new_dynamic.__capacity * new_dynamic.__grow_factor
+        new_dynamic.__chunk += [None] * (new_dynamic.__capacity - self.__capacity)
+    if element is None:
+        pass
+    else:
+        new_dynamic.__chunk[new_dynamic.__size] = element
+    new_dynamic.__size += 1
+    return new_dynamic
+
+def intersection(self, pos: int, value: Optional[int]) -> 'DynamicArray':
+    """ Add an element into the array at specified position
+    :param pos: Index of the array
+    :param value: The value of element, int or None
+    """
+    if pos < 0 or pos >= self.__size:
+        raise Exception('Out of the index')
+    new_dynamic = copy_self(self)
+    new_dynamic.__chunk[pos] = value
+    return new_dynamic
+
+def remove(self, pos: int) -> 'DynamicArray':
+    """ Remove an element of array at specified position
+    :param pos: Index of the array
+    """
+    new_dynamic = self.copy()
+    if pos < 0 or pos >= self.__size:
+        raise Exception('Out of the index')
+    while pos < self.__size - 1:
+        new_dynamic.__chunk[pos] = new_dynamic.__chunk[pos + 1]
+        pos += 1
+    new_dynamic.__chunk[pos] = None
+    new_dynamic.__size -= 1
+    return new_dynamic
+
+def length(self) -> int:
+    """ Return the length of array. """
+    return self.__size
+
+def member(self, value: int) -> bool:
+    """ Determines whether the given value is a member of the array.
+
+    :param value: The given value.
+    :return: Value is member if return True, else not.
+    """
+    return self.__chunk.__contains__(value)
+
+def reverse(self) -> 'DynamicArray':
+    """ Reverse the array. """
+    new_dynamic = copy.deepcopy(self)
+    left = 0
+    right = self.__size - 1
+    while left < right:
+        t = new_dynamic.__chunk[right]
+        new_dynamic.__chunk[right] = new_dynamic.__chunk[left]
+        new_dynamic.__chunk[left] = t
+        right -= 1
+        left += 1
+    return new_dynamic
+
+def filter(self, predicate: Callable[[Optional[int]], bool]) -> 'DynamicArray':
+    """ Filter the array by specific predicate. """
+    new_dynamic = copy.deepcopy(self)
+    for i in range(new_dynamic.__size - 1, -1, -1):
+        if not predicate(new_dynamic.__chunk[i]):
+            new_dynamic.remove(i)
+    return new_dynamic
+
+def map(self, function: Callable[[Any], int]) -> 'DynamicArray':
+    """ Applies a function to each element in a dynamic array"""
+    new_dynamic = copy.deepcopy(self)
+    i = 0
+    while i < new_dynamic.length():
+        new_dynamic.__chunk[i] = function(new_dynamic.__chunk[i])
+    return new_dynamic
+
+ def reduce(self, function: Callable[[Optional[int], Optional[int]], int],
+               initial_state: Optional[int] = None) -> Optional[int]:
+    """ Apply function of two arguments cumulatively to the items of the array,
+    from left to right, to reduce the array to a single value.
+    """
+    if length(self) == 0:
+        return None
+    state = initial_state
+    for element in self.__chunk:
+        if element is not None:
+            state = function(state, element)
+        else:
+            break
+    return state
+
+def find(self, p: Callable[[Optional[int]], bool]) -> bool:
+    """"""
+    for k in self.__chunk:
+        if p(k):
+            return True
+    return False
+
+
+
 def empty() -> 'DynamicArray':
     """clean all data from the datastruct"""
     return DynamicArray(0)
@@ -65,138 +193,13 @@ class DynamicArray(object):
         self.__capacity = capacity
         self.__chunk: List[Optional[int]] = [None] * self.__capacity
 
-    def iterator(self) -> 'DArrayIterator':
-        """
-        Convert a dynamic array to an iterator
-        :return: an iterator
-        """
-        return DArrayIterator(self.__chunk)
-
-    def copy(self) -> 'DynamicArray':
-        """
-        Make a copy of self
-        :return:
-        """
-        new_dynamic = copy.deepcopy(self)
-        return new_dynamic
-
-    def to_list(self) -> List[int]:
-        """ Transform the array to a list """
-        lst: List[int] = []
-        for i in self.__chunk:
-            if i is not None:
-                lst.append(i)
-            else:
-                break
-        return lst
-
-    def cons(self, element: Optional[int]) -> 'DynamicArray':
-        """ Add an element at the end of the array"""
-        new_dynamic = self.copy()
-        if new_dynamic.__size == new_dynamic.__capacity:
-            new_dynamic.__capacity = new_dynamic.__capacity * new_dynamic.__grow_factor
-            new_dynamic.__chunk += [None] * (new_dynamic.__capacity - self.__capacity)
-        if element is None:
-            pass
-        else:
-            new_dynamic.__chunk[new_dynamic.__size] = element
-        new_dynamic.__size += 1
-        return new_dynamic
-
-    def intersection(self, pos: int, value: Optional[int]) -> 'DynamicArray':
-        """ Add an element into the array at specified position
-        :param pos: Index of the array
-        :param value: The value of element, int or None
-        """
-        if pos < 0 or pos >= self.__size:
-            raise Exception('Out of the index')
-        new_dynamic = self.copy()
-        new_dynamic.__chunk[pos] = value
-        return new_dynamic
-
-    def remove(self, pos: int) -> 'DynamicArray':
-        """ Remove an element of array at specified position
-        :param pos: Index of the array
-        """
-        new_dynamic = self.copy()
-        if pos < 0 or pos >= self.__size:
-            raise Exception('Out of the index')
-        while pos < self.__size - 1:
-            new_dynamic.__chunk[pos] = new_dynamic.__chunk[pos + 1]
-            pos += 1
-        new_dynamic.__chunk[pos] = None
-        new_dynamic.__size -= 1
-        return new_dynamic
-
-    def length(self) -> int:
-        """ Return the length of array. """
-        return self.__size
-
-    def member(self, value: int) -> bool:
-        """ Determines whether the given value is a member of the array.
-
-        :param value: The given value.
-        :return: Value is member if return True, else not.
-        """
-        return self.__chunk.__contains__(value)
-
-    def reverse(self) -> 'DynamicArray':
-        """ Reverse the array. """
-        new_dynamic = copy.deepcopy(self)
-        left = 0
-        right = self.__size - 1
-        while left < right:
-            t = new_dynamic.__chunk[right]
-            new_dynamic.__chunk[right] = new_dynamic.__chunk[left]
-            new_dynamic.__chunk[left] = t
-            right -= 1
-            left += 1
-        return new_dynamic
-
-    def filter(self, predicate: Callable[[Optional[int]], bool]) -> 'DynamicArray':
-        """ Filter the array by specific predicate. """
-        new_dynamic = copy.deepcopy(self)
-        for i in range(new_dynamic.__size - 1, -1, -1):
-            if not predicate(new_dynamic.__chunk[i]):
-                new_dynamic.remove(i)
-        return new_dynamic
-
-    def map(self, function: Callable[[Any], int]) -> 'DynamicArray':
-        """ Applies a function to each element in a dynamic array"""
-        new_dynamic = copy.deepcopy(self)
-        i = 0
-        while i < new_dynamic.length():
-            new_dynamic.__chunk[i] = function(new_dynamic.__chunk[i])
-        return new_dynamic
-
-    def reduce(self, function: Callable[[Optional[int], Optional[int]], int],
-               initial_state: Optional[int] = None) -> Optional[int]:
-        """ Apply function of two arguments cumulatively to the items of the array,
-            from left to right, to reduce the array to a single value.
-        """
-        if self.length() == 0:
-            return None
-        state = initial_state
-        for element in self.__chunk:
-            if element is not None:
-                state = function(state, element)
-            else:
-                break
-        return state
-
-    def find(self, p: Callable[[Optional[int]], bool]) -> bool:
-        """"""
-        for k in self.__chunk:
-            if p(k):
-                return True
-        return False
 
     def __eq__(self, other: 'DynamicArray') -> bool:
         """
         Return True if those tow DynamicArray is equal
         All built-in properties should be equal
         """
-        if self.__size != other.length() or self.__capacity != other.__capacity:
+        if self.__size != length(other) or self.__capacity != other.__capacity:
             return False
         for a, b in zip(self.__chunk, other.__chunk):
             if a != b:
