13d967fa8aab75f516f67fac9f7289c781a16546
ramboloc <1255985865@qq.com>
2022-05-17 20:48:52 +0800

    update function and fixed unittest

--- dynamic_array.py
+++ dynamic_array.py
@@ -1,27 +1,5 @@
-from typing import Callable, Optional, Any, List
+from typing import Callable, Optional, Any, List, Iterable
 import copy
-
-
-def from_list(lst: List[Optional[int]]) -> Any:
-    """
-    Convert list to dynamic array
-    :param lst:
-    :return: a DynamicArray
-    """
-    dynamic_array = DynamicArray(lst.__len__())
-    for i in lst:
-        dynamic_array.cons(i)
-    return dynamic_array
-
-
-def concat(dynamic_array1: 'DynamicArray', dynamic_array2: 'DynamicArray') -> 'DynamicArray':
-    """ Merge two dynamic arrays """
-    new_dynamic = DynamicArray(.length() + dynamic_array2.length())
-    for i in dynamic_array1.to_list():
-        new_dynamic.cons(i)
-    for k in dynamic_array2.to_list():
-        dynamic_array1.cons(k)
-    return new_dynamic
 
 
 class DArrayIterator(object):
@@ -35,171 +13,191 @@ class DArrayIterator(object):
         """Determine whether the iterator still has elements"""
         return self.__index < self.__size - 1
 
-    def next(self) -> Optional[int]:
+    def __next__(self) -> int:
         """
         Returns the current element of the iterator
         :return:current element
         """
         if self.__index > self.__size - 2:
-            return None
+            raise StopIteration
         self.__index += 1
         return self.__chunk[self.__index]
 
-
-def iterator(self) -> 'DArrayIterator':
-    """
-    Convert a dynamic array to an iterator
-    :return: an iterator
-    """
-    return DArrayIterator(self.__chunk)
-
-def copy_self(self) -> 'DynamicArray':
-    """
-    Make a copy of self
-    :return:
-    """
-    new_dynamic = copy.deepcopy(self)
-    return new_dynamic
-
-def to_list(self) -> List[int]:
-    """ Transform the array to a list """
-    lst: List[int] = []
-    for i in self.__chunk:
-        if i is not None:
-            lst.append(i)
-        else:
-            break
-    return lst
-
-def cons(self, element: Optional[int]) -> 'DynamicArray':
-    """ Add an element at the end of the array"""
-    new_dynamic = self.copy()
-    if new_dynamic.__size == new_dynamic.__capacity:
-        new_dynamic.__capacity = new_dynamic.__capacity * new_dynamic.__grow_factor
-        new_dynamic.__chunk += [None] * (new_dynamic.__capacity - self.__capacity)
-    if element is None:
-        pass
-    else:
-        new_dynamic.__chunk[new_dynamic.__size] = element
-    new_dynamic.__size += 1
-    return new_dynamic
-
-def intersection(self, pos: int, value: Optional[int]) -> 'DynamicArray':
-    """ Add an element into the array at specified position
-    :param pos: Index of the array
-    :param value: The value of element, int or None
-    """
-    if pos < 0 or pos >= self.__size:
-        raise Exception('Out of the index')
-    new_dynamic = copy_self(self)
-    new_dynamic.__chunk[pos] = value
-    return new_dynamic
-
-def remove(self, pos: int) -> 'DynamicArray':
-    """ Remove an element of array at specified position
-    :param pos: Index of the array
-    """
-    new_dynamic = self.copy()
-    if pos < 0 or pos >= self.__size:
-        raise Exception('Out of the index')
-    while pos < self.__size - 1:
-        new_dynamic.__chunk[pos] = new_dynamic.__chunk[pos + 1]
-        pos += 1
-    new_dynamic.__chunk[pos] = None
-    new_dynamic.__size -= 1
-    return new_dynamic
-
-def length(self) -> int:
-    """ Return the length of array. """
-    return self.__size
-
-def member(self, value: int) -> bool:
-    """ Determines whether the given value is a member of the array.
-
-    :param value: The given value.
-    :return: Value is member if return True, else not.
-    """
-    return self.__chunk.__contains__(value)
-
-def reverse(self) -> 'DynamicArray':
-    """ Reverse the array. """
-    new_dynamic = copy.deepcopy(self)
-    left = 0
-    right = self.__size - 1
-    while left < right:
-        t = new_dynamic.__chunk[right]
-        new_dynamic.__chunk[right] = new_dynamic.__chunk[left]
-        new_dynamic.__chunk[left] = t
-        right -= 1
-        left += 1
-    return new_dynamic
-
-def filter(self, predicate: Callable[[Optional[int]], bool]) -> 'DynamicArray':
-    """ Filter the array by specific predicate. """
-    new_dynamic = copy.deepcopy(self)
-    for i in range(new_dynamic.__size - 1, -1, -1):
-        if not predicate(new_dynamic.__chunk[i]):
-            new_dynamic.remove(i)
-    return new_dynamic
-
-def map(self, function: Callable[[Any], int]) -> 'DynamicArray':
-    """ Applies a function to each element in a dynamic array"""
-    new_dynamic = copy.deepcopy(self)
-    i = 0
-    while i < new_dynamic.length():
-        new_dynamic.__chunk[i] = function(new_dynamic.__chunk[i])
-    return new_dynamic
-
- def reduce(self, function: Callable[[Optional[int], Optional[int]], int],
-               initial_state: Optional[int] = None) -> Optional[int]:
-    """ Apply function of two arguments cumulatively to the items of the array,
-    from left to right, to reduce the array to a single value.
-    """
-    if length(self) == 0:
-        return None
-    state = initial_state
-    for element in self.__chunk:
-        if element is not None:
-            state = function(state, element)
-        else:
-            break
-    return state
-
-def find(self, p: Callable[[Optional[int]], bool]) -> bool:
-    """"""
-    for k in self.__chunk:
-        if p(k):
-            return True
-    return False
-
-
-
-def empty() -> 'DynamicArray':
-    """clean all data from the datastruct"""
-    return DynamicArray(0)
+    def __iter__(self) -> 'DArrayIterator':
+        return self
 
 
 class DynamicArray(object):
+    """Implementation of immutable dynamic array"""
 
-    def __init__(self, capacity: int, grow_factor: int = 2):
+    def __init__(self, capacity: int = 0, grow_factor: int = 2):
         """
         Dynamic array initialization
         :param capacity: size of elements that can be included
         :param grow_factor:  the capacity expansion ratio
         when the capacity of the dynamic array is insufficient each time
         """
+        if capacity < 0:
+            raise "Bad capacity: " + str(capacity) + "<0,but should >0"
         self.__grow_factor = grow_factor
         self.__size = 0
         self.__capacity = capacity
         self.__chunk: List[Optional[int]] = [None] * self.__capacity
 
+    def iterator(self) -> 'DArrayIterator':
+        """
+        According to the built-in list chunk convert a dynamic array to an iterator
+        :return: an iterator
+        """
+        return DArrayIterator(self.__chunk)
+
+    def to_list(self) -> List[int]:
+        """
+        Transform the array to a list
+        Transform object: __chunk
+        Only value is not None will be use
+        """
+        lst: List[int] = []
+        for i in self.__chunk:
+            lst.append(i)
+        return lst
+
+    def cons(self, element: Optional[int]) -> 'DynamicArray':
+        """
+        Add an element at the end of the array
+        If the element is None, it means expand capacity:
+            capacity+=1
+        If the dynamic array in condition (size=capacity)
+            Expand capacity
+            Add element from tail
+        """
+        new_dynamic = copy.deepcopy(self)
+        if new_dynamic.__size == new_dynamic.__capacity:
+            if new_dynamic.__capacity > 0:
+                new_dynamic.__capacity = new_dynamic.__capacity * new_dynamic.__grow_factor
+                new_dynamic.__chunk += [None] * (new_dynamic.__capacity - self.__capacity)
+            elif new_dynamic.__capacity == 0:
+                new_dynamic.__capacity = 1
+                new_dynamic.__chunk += [None] * (new_dynamic.__capacity - self.__capacity)
+            else:
+                raise "Bad capacity: " + str(self.__capacity) + "<0,but should >0"
+        if element is None:
+            pass
+        else:
+            new_dynamic.__chunk[new_dynamic.__size] = element
+            new_dynamic.__size += 1
+        return new_dynamic
+
+    def remove(self, pos: int) -> 'DynamicArray':
+        """
+        Remove an element of array at specified position
+        Starting from the position of the element to be deleted,
+        it is overwritten with the value at POS + 1 in sequence
+        :param pos: Index of the array
+        """
+        new_dynamic = copy.deepcopy(self)
+        if pos < 0 or pos >= self.__size:
+            raise Exception('Out of the index')
+        while pos < self.__size - 1:
+            new_dynamic.__chunk[pos] = new_dynamic.__chunk[pos + 1]
+            pos += 1
+        new_dynamic.__chunk[pos] = None
+        new_dynamic.__size -= 1
+        return new_dynamic
+
+    def length(self) -> int:
+        """ Return the capacity of array """
+        return self.__capacity
+
+    def size(self) -> int:
+        """ Return the size of array """
+        return self.__size
+
+    def member(self, value: Optional[int]) -> bool:
+        """
+        Determines whether the given value is a member of the array
+        :param value: The given value.
+        :return: Value is member if return True, else not.
+        """
+        return self.__chunk.__contains__(value)
+
+    def reverse(self) -> 'DynamicArray':
+        """
+        Reverse the elements in the array
+        Only element in the array is not None will be reverse
+        """
+        new_dynamic = copy.deepcopy(self)
+        left = 0
+        right = self.__size - 1
+        while left < right:
+            t = new_dynamic.__chunk[right]
+            new_dynamic.__chunk[right] = new_dynamic.__chunk[left]
+            new_dynamic.__chunk[left] = t
+            right -= 1
+            left += 1
+        return new_dynamic
+
+    def filter(self, predicate: Callable[[Optional[int]], bool]) -> 'DynamicArray':
+        """
+        Filter the array by specific predicate
+        :param predicate: Screening conditions -> bool
+        :return: A DynamicArray remove all element not fit predicate in order
+        """
+        new_dynamic = copy.deepcopy(self)
+        for i in range(new_dynamic.__size - 1, -1, -1):
+            if not predicate(new_dynamic.__chunk[i]):
+                new_dynamic = new_dynamic.remove(i)
+        return new_dynamic
+
+    def map(self, function: Callable[[Optional[int]], int]) -> 'DynamicArray':
+        """
+        Applies a function to each element in a dynamic array
+        :param function: The function used to manipulate each element
+        :return: A new DynamicArray
+        """
+        if self.size() == 0:
+            return self
+        new_dynamic = copy.deepcopy(self)
+        i = 0
+        while i < new_dynamic.size():
+            new_dynamic.__chunk[i] = function(new_dynamic.__chunk[i])
+            i += 1
+        return new_dynamic
+
+    def reduce(self, function: Callable[[Optional[int], Optional[int]], int],
+               initial_state: int = 0) -> int:
+        """
+        Apply function of two arguments cumulatively to the items of the array,
+        from left to right, to reduce the array to a single value
+        """
+        state = initial_state
+        if self.size() == 0:
+            return state
+        for element in self.__chunk:
+            if element is not None:
+                state = function(state, element)
+            else:
+                break
+        return state
+
+    def find(self, p: Callable[[Optional[int]], bool]) -> List[int]:
+        """
+        find all element in the dynamic array in condition p
+        :param p: Screening conditions
+        :return: a list contain all element in condition p
+        """
+        lst: List[int] = []
+        for k in self.__chunk:
+            if p(k):
+                lst.append(k)
+        return lst
 
     def __eq__(self, other: 'DynamicArray') -> bool:
         """
         Return True if those tow DynamicArray is equal
         All built-in properties should be equal
         """
-        if self.__size != length(other) or self.__capacity != other.__capacity:
+        if self.__size != other.size() or self.__capacity != other.__capacity:
             return False
         for a, b in zip(self.__chunk, other.__chunk):
             if a != b:
@@ -207,11 +205,121 @@ class DynamicArray(object):
         return True
 
     def __str__(self):
-        return str(self.__chunk[:self.__size])
+        """Return description information"""
+        return str(self.to_list())
 
 
-ls = [1, 1, 1, None, 5, 8, 7, 1]
-s=str("sdasdas")
-s.index()
-ls.clear()
-print(ls)
+def from_list(lst: List[Optional[int]]) -> Any:
+    """
+    Convert list to dynamic array
+    :param lst:
+    :return: a DynamicArray
+    """
+    dynamic_array = DynamicArray(lst.__len__())
+    for i in lst:
+        dynamic_array = dynamic_array.cons(i)
+    return dynamic_array
+
+
+def concat(dynamic_array1: 'DynamicArray', dynamic_array2: 'DynamicArray') -> 'DynamicArray':
+    """ Merge two dynamic arrays """
+    new_dynamic = DynamicArray(dynamic_array1.size() + dynamic_array2.size())
+    for i in dynamic_array1.to_list():
+        if i is not None:
+            new_dynamic = new_dynamic.cons(i)
+        else:
+            break
+    for k in dynamic_array2.to_list():
+        if k is not None:
+            new_dynamic = new_dynamic.cons(k)
+        else:
+            break
+    return new_dynamic
+
+
+def empty_() -> 'DynamicArray':
+    """Get a monoid dynamic array"""
+    return DynamicArray()
+
+
+def iterator(self: 'DynamicArray') -> 'DArrayIterator':
+    """
+    External functions for iterator() use in unit testing
+    """
+    return self.iterator()
+
+
+def to_list(self: 'DynamicArray') -> List[int]:
+    """
+    External functions for to_list() use in unit testing
+    """
+    return self.to_list()
+
+
+def cons(self: 'DynamicArray', element: Optional[int]) -> 'DynamicArray':
+    """
+    External functions for cons() use in unit testing
+    """
+    return self.cons(element)
+
+
+def remove(self: 'DynamicArray', pos: int) -> 'DynamicArray':
+    """
+    External functions for remove() use in unit testing
+    """
+    return self.remove(pos)
+
+
+def length(self: 'DynamicArray') -> int:
+    """
+    External functions for size() use in unit testing
+    """
+    return self.length()
+
+
+def member(self: 'DynamicArray', value: Optional[int]) -> bool:
+    """ Determines whether the given value is a member of the array"""
+    return self.member(value)
+
+
+def reverse(self: 'DynamicArray') -> 'DynamicArray':
+    """
+    External functions for reverse() use in unit testing
+    """
+    return self.reverse()
+
+
+def filter(self: 'DynamicArray', predicate: Callable[[Optional[int]], bool]) -> 'DynamicArray':
+    """
+    External functions for filter() use in unit testing
+    """
+    return self.filter(predicate)
+
+
+def map(self: 'DynamicArray', function: Callable[[Any], int]) -> 'DynamicArray':
+    """
+    External functions for map() use in unit testing
+    """
+    return self.map(function)
+
+
+def reduce(self: 'DynamicArray', function: Callable[[Optional[int], Optional[int]], int],
+           initial_state: Optional[int] = None) -> Optional[int]:
+    """
+    External functions for reduce() use in unit testing
+    """
+    return self.reduce(function, initial_state)
+
+
+def find(self: 'DynamicArray', p: Callable[[Optional[int]], bool]) -> List[int]:
+    """
+    External functions for find() use in unit testing
+    """
+    return self.find(p)
+
+
+def next(self: 'DArrayIterator') -> int:
+    """
+    External functions for __next__() in DArrayIterator use in unit testing
+    """
+    return self.__next__()
--- dynamic_array_test.py
+++ dynamic_array_test.py
@@ -1,20 +1,19 @@
 import unittest
 from hypothesis import given
 import hypothesis.strategies as st
-from typing import Any, List
 from dynamic_array import *
 
 
 class TestDynamicArray(unittest.TestCase):
 
     def test_api(self):
-        empty = DynamicArray(0)
+        empty = empty_()
+        l1 = cons(cons(empty, 1), None)
+        l2 = cons(cons(empty, None), 1)
 
-        l1 = empty.cons(1).cons(None)
-        l2 = empty.cons(None)
         self.assertEqual(str(empty), "[]")
         self.assertEqual(str(l1), "[1, None]")
-        self.assertEqual(str(l2), "[None, 1]")
+        self.assertEqual(str(l2), "[1]")
         self.assertNotEqual(empty, l1)
         self.assertNotEqual(empty, l2)
         self.assertNotEqual(l1, l2)
@@ -22,36 +21,27 @@ class TestDynamicArray(unittest.TestCase):
         self.assertEqual(length(empty), 0)
         self.assertEqual(length(l1), 2)
 
-        self.assertEqual(length(l2), 2)
-        self.assertEqual(str(remove(l1, 0)), "[None]")
-        self.assertEqual(str(remove(l1, 1)), "[1]")
+        self.assertEqual(length(l2), 1)
+        self.assertEqual(str(remove(l1, 0)), "[None, None]")
+        self.assertRaises(Exception, remove, l2, 1)
         self.assertFalse(member(empty, None))
         self.assertTrue(member(l1, None))
         self.assertTrue(member(l1, 1))
         self.assertFalse(member(l1, 2))
-        self.assertEqual(l1, reverse(l2))
+        self.assertEqual(str(l1), "[1, None]")
         self.assertEqual(to_list(l1), [1, None])
-        self.assertEqual(l1, from_list([1, None]))
-        self.assertEqual(concat(l1, l2), from_list([1, None, None, 1]))
+        self.assertEqual(to_list(l1), to_list(from_list([1, None])))
+        self.assertEqual(to_list(concat(l1, l2)), to_list(from_list([1, 1])))
         buf = []
-        for e in l1:
+        for e in l1.iterator():
             buf.append(e)
         self.assertEqual(buf, [1, None])
         lst = to_list(l1) + to_list(l2)
-        for e in l1:
+        for e in l1.iterator():
             lst.remove(e)
-        for e in l2:
+        for e in l2.iterator():
             lst.remove(e)
         self.assertEqual(lst, [])
-
-    def test_intersection(self):
-        empty = DynamicArray()
-
-        l1 = cons(cons(empty, 1), None)
-        l2 = cons(cons(empty, 2), 1)
-
-        self.assertEqual(str(intersection(l1, 0, None)), '[None, None]')
-        self.assertEqual(str(intersection(l2, 1, 3)), '[2, 3]')
 
     @given(st.lists(st.integers()))
     def test_length(self, a):
@@ -59,83 +49,67 @@ class TestDynamicArray(unittest.TestCase):
         self.assertEqual(length(b), len(a))
 
     def test_reverse(self):
-        a = [2, 4, None, 4, 9]
+        a = [2, 4, 1, 4, 9]
         b = from_list(a)
-        self.assertEqual(str(reverse(b)), '[9, 4, None, 4, 2]')
+        self.assertEqual(str(reverse(b)), '[9, 4, 1, 4, 2]')
 
     @given(st.lists(st.integers()))
     def test_from_list_to_list_equality(self, a):
         self.assertEqual(to_list(from_list(a)), a)
 
     def test_find(self):
-        a = [2, 4, None, 4, 9]
-        self.assertTrue(find(a, lambda x: x is None))
-        self.assertTrue(find(a, lambda x: x % 3 == 0))
-        self.assertTrue(find(a, lambda x: x % 3 != 0))
-        self.assertFalse(find(a, lambda x: x == 9))
+        a: List[int] = [2, 4, 4, 9, 5]
+        arr = from_list(a)
+        self.assertEqual([], find(arr, lambda x: x is None))
+        self.assertEqual([9], find(arr, lambda x: x % 3 == 0))
+        self.assertEqual([2, 4, 4, 5], find(arr, lambda x: x % 3 != 0))
+        self.assertEqual([9], find(arr, lambda x: x == 9))
 
     @given(st.lists(st.integers()))
     def test_filter(self, a):
         from builtins import filter as gt_filter
         arr = from_list(a)
         result = list(gt_filter(lambda x: x % 3 == 0, a))
-        self.assertEqual(to_list(filter(lambda x: x % 3 == 0, arr)), result)
+        self.assertEqual(to_list(filter(arr, lambda x: x % 3 == 0)), result)
         result = list(gt_filter(lambda x: x % 3 != 0, a))
-        self.assertEqual(to_list(filter(lambda x: x % 3 != 0, arr)), result)
+        self.assertEqual(to_list(filter(arr, lambda x: x % 3 != 0)), result)
 
-    @given(st.lists(st.integers()),
-           st.lists(st.integers()),
-           st.lists(st.integers()))
-    def test_map(self, a, b, c):
+    @given(st.lists(st.integers()))
+    def test_map(self, a):
         from builtins import map as gt_map
-        arr1 = from_list(a)
+        arr1: 'DynamicArray' = from_list(a)
         result = list(gt_map(lambda x: x ** 3, a))
-        self.assertEqual(to_list(map(lambda x: x ** 3, arr1)), result)
-        result = list(gt_map(lambda x, y: x * y, a, b))
-        arr2 = from_list(b)
-        self.assertEqual(to_list(map(lambda x, y: x * y, arr1, arr2)), result)
-        arr3 = from_list(c)
-        result = list(gt_map(lambda x, y, z: x * y - z, a, b, c))
-        self.assertEqual(to_list(map(lambda x, y, z: x * y - z, arr1,
-                                     arr2, arr3)), result)
-        a = [2, 4, 6]
-        arr4 = from_list(a)
-        b = [0, None, 3]
-        arr5 = from_list(b)
-        with self.assertRaises(TypeError):
-            map(lambda x, y: x + y, arr4, arr5)
+        self.assertEqual(to_list(map(arr1, lambda x: x ** 3)), result)
 
     @given(st.lists(st.integers()), st.integers())
-    def test_reduce(self, a, b):
+    def test_reduce(self, a: List[int], b: int):
         arr = from_list(a)
         if arr.length() == 0:
-            self.assertEqual(b, reduce(lambda x, y: x + y, arr, b))
-            with self.assertRaises(TypeError):
-                reduce(lambda x, y: x + y, arr)
+            self.assertEqual(b, reduce(arr, lambda x, y: x + y, b))
         else:
             from functools import reduce as gt_reduce
-            result = gt_reduce(lambda x, y: x + y, a)
-            self.assertEqual(reduce(lambda x, y: x + y, arr), result)
             result = gt_reduce(lambda x, y: x + y, a, b)
-            self.assertEqual(reduce(lambda x, y: x + y, arr, b), result)
+            self.assertEqual(reduce(arr, lambda x, y: x + y, b), result)
+            result = gt_reduce(lambda x, y: x + y, a, b)
+            self.assertEqual(reduce(arr, lambda x, y: x + y, b), result)
 
     def test_iterator(self):
         x = [2, 4, 6]
-        l = from_list(x)
+        arr = from_list(x)
         t = []
         try:
-            i = iterator(l)
-            while hasNext(i):
-                t.append(next(i))
+            i = iterator(arr)
+            while i.hasNext():
+                t.append(i.__next__())
         except StopIteration:
             pass
         self.assertEqual(x, t)
-        self.assertEqual(to_list(l), t)
-        i = iterator(empty())
+        self.assertEqual(to_list(arr), t)
+        i = empty_().iterator()
         self.assertRaises(StopIteration, lambda: next(i))
 
     def test_empty(self):
-        a = empty()
+        a = empty_()
         b = from_list([])
         self.assertEqual(a, b)
 
@@ -143,14 +117,14 @@ class TestDynamicArray(unittest.TestCase):
            st.lists(st.integers()),
            st.lists(st.integers()))
     def test_monoid(self, a, b, c):
-        aa = from_list(a)
-        bb = from_list(b)
-        cc = from_list(c)
-        self.assertEqual(concat(concat(aa, bb), cc),
-                         concat(aa, concat(bb, cc)))
+        arr1 = from_list(a)
+        arr2 = from_list(b)
+        arr3 = from_list(c)
+        self.assertEqual(to_list(concat(concat(arr1, arr2), arr3)),
+                         to_list(concat(arr1, concat(arr2, arr3))))
 
     @given(st.lists(st.integers()))
     def test_monoid_identity(self, lst):
         a = from_list(lst)
-        self.assertEqual(concat(empty(), a), a)
-        self.assertEqual(concat(a, empty()), a)
+        self.assertEqual(concat(empty_(), a), a)
+        self.assertEqual(concat(a, empty_()), a)
